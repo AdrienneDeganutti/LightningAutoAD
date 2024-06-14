42edf42d639c12e7e11999093c5383c6641ba5fe
Adrienne Deganutti <ad01355@surrey.ac.uk>
2024-06-14 15:48:10 +0100

    add evaluation scorer

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dataset import MyDataModule
 from trainer import LightningModule
 from modeling.tokenizer.caption_tokenizer import TokenizerHandler
 from modeling.vision_transformer.model_ad import VideoCaptionModel
+from evalcap.evaluate_scores import EvaluateCaption
 from modeling.gpt_model import GPT2LMHeadModel
 from configs.load_config import get_custom_args
 
@@ -14,12 +15,17 @@ def main(args):
     tokenizer = TokenizerHandler()
     transformer_model = VideoCaptionModel(num_latents=args.max_seq_length)
     gpt_model = GPT2LMHeadModel.from_pretrained("gpt2")
+    scorer = EvaluateCaption(args, tokenizer)
 
     data_module = MyDataModule(args)
     
-    model = LightningModule(args, transformer_model, gpt_model, tokenizer)
+    model = LightningModule(args, transformer_model, gpt_model, tokenizer, scorer)
+
+
 
     trainer = Trainer(
+        accelerator='gpu', 
+        devices=1,
         max_epochs=args.max_epochs
     )
 
