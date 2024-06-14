c5245fdfb364bced5f0c1fc9c0b41f5c106ef1eb
Adrienne Deganutti <ad01355@surrey.ac.uk>
2024-06-14 15:47:40 +0100

    add validation step

--- trainer.py
+++ trainer.py
@@ -4,21 +4,18 @@ from torch.optim import AdamW
 import pytorch_lightning as pl
 
 class LightningModule(pl.LightningModule):
-    def __init__(self, args, transformer_model, gpt_model, tokenizer):
+    def __init__(self, args, transformer_model, gpt_model, tokenizer, scorer):
         super().__init__()
         self.args = args
         self.transformer = transformer_model
         self.gpt = gpt_model
         self.tokenizer = tokenizer
-        
-        #Combine parameters and initialize optimizer
-        combined_parameters = list(self.transformer.parameters()) + list(self.gpt.parameters())
-        self.optimizer = AdamW(combined_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+        self.scorer = scorer
         
     
     def forward(self, tokenized_caption, visual_frame):
         prefix_vector = self.transformer(visual_frame)
-        gpt_output = self.gpt(input_embeds=prefix_vector, labels=tokenized_caption)
+        gpt_output = self.gpt(inputs_embeds=prefix_vector, labels=tokenized_caption)
         loss, logits = gpt_output[:2]
         return loss, logits
     
@@ -39,9 +36,20 @@ class LightningModule(pl.LightningModule):
         return loss
     
     def validation_step(self, batch, batch_idx):
-        y_hat = self(x)
-        loss = self.criterion(y_hat, y)
-        self.log('val_loss', loss)
+        img_ID, caption, visual_frame = batch
+        #tokenized_caption = self.tokenizer.tokenize_caption(self.args, caption)
+
+        prefix_vector = self.transformer(visual_frame)
+        outputs = self.gpt(inputs_embeds=prefix_vector, )      #No caption given for validation
+        logits = outputs[0]
+
+        batch_acc = self.scorer.compute_score(img_ID, logits, caption)
+
+        self.log('validation_cider', batch_acc)
+        
+        return batch_acc
     
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        #Combine parameters and initialize optimizer
+        combined_parameters = list(self.transformer.parameters()) + list(self.gpt.parameters())
+        return AdamW(combined_parameters, lr=self.args.learning_rate, eps=self.args.adam_epsilon)
